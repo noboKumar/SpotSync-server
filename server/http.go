59180e8444f8460daad81e6199587d462c601f3a
noboKumar <nobokumar227@gmail.com>
2026-06-26 20:54:58 +0600

    add: Start function on http

--- server/http.go
+++ server/http.go
@@ -18,14 +18,21 @@ func (cv *CustomValidator) Validate(i interface{}) error {
 
 func Start(cfg *config.Config, db *gorm.DB) {
 	db.AutoMigrate()
+
 	e := echo.New()
 	e.Validator = &CustomValidator{validator: validator.New()}
 
 	//middleware
+
+	
 	e.GET("/health", func(c *echo.Context) error {
 		return c.JSON(200, map[string]string{
 			"status": "ok",
 		})
 	})
+
+	//routes
+
+	e.Start(":" + cfg.Port)
 
 }
