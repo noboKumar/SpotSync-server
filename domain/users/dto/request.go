d01cb402641ad055d1c9fc3d95a8419cc3b5f15e
noboKumar <nobokumar227@gmail.com>
2026-06-27 21:54:13 +0600

    add: user req dto

--- domain/users/dto/request.go
+++ domain/users/dto/request.go
@@ -1,1 +1,16 @@
 package dto
+
+type RegisterRequest struct {
+	Username string `json:"username" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type RefreshTokenRequest struct {
+	Token string `json:"refresh_token" validate:"required"`
+}
