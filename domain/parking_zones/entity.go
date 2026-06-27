9dd5eb17106e44eeee0bdfce596038025146d1d6
noboKumar <nobokumar227@gmail.com>
2026-06-28 04:20:39 +0600

    add: parking zone entity model

--- domain/parking_zones/entity.go
+++ domain/parking_zones/entity.go
@@ -1,1 +1,15 @@
 package parkingzones
+
+import (
+	"time"
+)
+
+type ParkingZone struct {
+	ID             uint      `gorm:"primaryKey"`
+	Name           string    `gorm:"not null"`
+	Type           string    `gorm:"type:varchar(20);not null"`
+	Total_Capacity int       `gorm:"not null"`
+	Price_per_Hour float64   `gorm:"not null"`
+	CreatedAt      time.Time `gorm:"autoCreateTime"`
+	UpdatedAt      time.Time `gorm:"autoUpdateTime"`
+}
--- domain/users/entity.go
+++ domain/users/entity.go
@@ -4,11 +4,9 @@ import (
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
-	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
 	ID        uint      `gorm:"primaryKey"`
 	Name      string    `gorm:"not null"`
 	Email     string    `gorm:"unique;not null"`
