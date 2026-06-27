6f59564823bfbad98bac9e9f5d62af4ff1297fbf
noboKumar <nobokumar227@gmail.com>
2026-06-27 21:56:23 +0600

    add: user dto response

--- domain/users/dto/response.go
+++ domain/users/dto/response.go
@@ -1,1 +1,10 @@
 package dto
+
+type UserResponse struct {
+	ID           string `json:"id"`
+	Username     string `json:"username"`
+	Email        string `json:"email"`
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	CreatedAt    string `json:"created_at"`
+}
