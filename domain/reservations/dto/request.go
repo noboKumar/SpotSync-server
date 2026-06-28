e8ec66fa606a0f749461a2afd7aaf51ac5f2cfac
noboKumar <nobokumar227@gmail.com>
2026-06-28 18:18:20 +0600

    add: reservation dto req and res

--- domain/reservations/dto/request.go
+++ domain/reservations/dto/request.go
@@ -1,1 +1,6 @@
 package dto
+
+type ReserveParkingZoneReq struct {
+	ZoneId       uint   `json:"zone_id" validate:"required"`
+	LicensePlate string `json:"license_plate" validate:"required"`
+}
--- domain/reservations/dto/response.go
+++ domain/reservations/dto/response.go
@@ -1,1 +1,19 @@
 package dto
+
+import "time"
+
+type ReserveParkingZoneRes struct {
+	ID           uint      `json:"id"`
+	UserID       uint      `json:"user_id"`
+	ZoneID       uint      `json:"zone_id"`
+	LicensePlate string    `json:"license_plate"`
+	Status       string    `json:"status"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type SuccessResponse struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
