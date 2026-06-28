53b8d247ee0f0d49719110d55fbb4d9df3e38864
noboKumar <nobokumar227@gmail.com>
2026-06-28 18:12:08 +0600

    add: reservation entity model

--- domain/reservations/entity.go
+++ domain/reservations/entity.go
@@ -1,1 +1,24 @@
 package reservations
+
+import (
+	"time"
+
+	parkingzones "github.com/noboKumar/SpotSync-server/domain/parking_zones"
+	"github.com/noboKumar/SpotSync-server/domain/users"
+)
+
+type Reservation struct {
+	ID uint `gorm:"primaryKey"`
+
+	UserID uint       `gorm:"not null"`
+	User   users.User `gorm:"foreignKey:UserID"`
+
+	ZoneID uint                     `gorm:"not null"`
+	Zone   parkingzones.ParkingZone `gorm:"foreignKey:ZoneID"`
+
+	LicensePlate string `gorm:"type:varchar(15) not null"`
+	SStatus      string `gorm:"type:varchar(20);default:'active';check:status IN ('active','completed','cancelled')"`
+
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+}
