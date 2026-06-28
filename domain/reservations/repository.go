70567eb38100b5ae8990a6844f5ddfcc42d4c176
noboKumar <nobokumar227@gmail.com>
2026-06-28 18:25:14 +0600

    add: reservation repository

--- domain/reservations/repository.go
+++ domain/reservations/repository.go
@@ -1,1 +1,25 @@
 package reservations
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	// reserve parking zone
+
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewReservationRepository(db *gorm.DB) Repository {
+	return &repository{db: db}
+}
+
+func (r *repository) ReserveParkingZone(reservation *Reservation) error {
+	result := r.db.Create(reservation)
+
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
